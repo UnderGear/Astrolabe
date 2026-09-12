786cb45df88a3f59048c28c3c0281fafe77da4ba
Christian Dell <christian.dell1@gmail.com>
2026-09-11 17:56:22 -0700

    fixed SpriteManager's OAM_ADDRESS access to use the namespace

--- source/SpriteManager.cpp
+++ source/SpriteManager.cpp
@@ -14,8 +14,8 @@ SpriteManager::SpriteManager(void* TileMemoryAddress)
     };
     SpriteBlockIterator = SpriteBlock->begin();
 
-    OAMMemory = reinterpret_cast<OAMT*>(OAM_ADDRESS);
-    OAMAffineMemory = reinterpret_cast<OAMAffineT*>(OAM_ADDRESS);
+    OAMMemory = reinterpret_cast<OAMT*>(MemoryMap::OAM_ADDRESS);
+    OAMAffineMemory = reinterpret_cast<OAMAffineT*>(MemoryMap::OAM_ADDRESS);
 
     //TODO: do this with an algo
     for (std::int32_t i{ MaxOAMs - 1 }; i >= 0; --i)
